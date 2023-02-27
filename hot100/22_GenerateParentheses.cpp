0f9441094e87a9f2a0e8e7ca14ed337731e35005
yrc <419107280@qq.com>
2023-02-27 14:25:02 +0800

    22_括号生成

---
+++ hot100/22_GenerateParentheses.cpp
@@ -0,0 +1,27 @@
+//
+// Created by sc on 2023/2/27.
+//
+
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<string> res;
+    void dfs(const string& tmp, int left, int right)
+    {
+        if(left<0 || left > right) return;
+
+        if(left == 0 && right == 0)
+        {
+            res.push_back(tmp);
+            return;
+        }
+
+        dfs(tmp + "(", left-1, right);
+        dfs(tmp + ")", left, right -1);
+    }
+    vector<string> generateParenthesis(int n) {
+        dfs("", n, n);
+        return res;
+    }
+};
