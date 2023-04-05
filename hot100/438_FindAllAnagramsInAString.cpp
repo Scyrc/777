61b89219cdef315f10e1b1e414fb060bf09dcf65
yrc <419107280@qq.com>
2023-04-05 14:06:27 +0800

    438_找到字符串中所有字母异位词

---
+++ hot100/438_FindAllAnagramsInAString.cpp
@@ -0,0 +1,49 @@
+//
+// Created by sc on 2023/4/5.
+//
+
+#include <vector>
+#include <string>
+#include <array>
+using std::vector;
+using std::string;
+using std::array;
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int lenS = s.size();
+        int lenP = p.size();
+        vector<int> res;
+        if(lenS < lenP) return res;
+        array<int, 26> countArr;
+        countArr.fill(0);
+        int diff = 0;
+        for(int i=0;i<lenP;++i)
+        {
+            ++countArr[s[i]-'a'];
+            --countArr[p[i]-'a'];
+        }
+        for(const int& x : countArr) diff = x != 0 ? diff+1 : diff;
+        if(diff == 0) res.push_back(0);
+
+        for(int i=0; i<lenS - lenP; ++i)
+        {
+            if(countArr[s[i]- 'a'] == 1) --diff;
+            if(countArr[s[i]- 'a'] == 0) ++diff;
+            --countArr[s[i]- 'a'];
+
+            if(countArr[s[i+lenP]- 'a'] == -1) --diff;
+            if(countArr[s[i+lenP]- 'a'] == 0) ++diff;
+            ++countArr[s[i+lenP]- 'a'];
+
+            if(diff == 0) res.push_back(i+1);
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution S;
+    S.findAnagrams("cbaebabacd", "abc");
+}
