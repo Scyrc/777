4d4523119e8342992b147c47b9372cd03c0bf557
yrc <419107280@qq.com>
2023-04-23 13:33:31 +0800

    剑指offer63_股票的最大利润

--- 剑指offer/63_股票的最大利润.cpp
+++ 剑指offer/63_股票的最大利润.cpp
@@ -1,3 +1,21 @@
 //
 // Created by sc on 2023/4/23.
 //
+#include <algorithm>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() < 2) return 0;
+        int minV = prices[0];
+        int res = 0;
+
+        for (int i = 1; i < prices.size(); ++i) {
+            if(prices[i] <= minV) minV = prices[i];
+            else res = std::max(res, prices[i] - minV);
+        }
+        return res;
+    }
+};
