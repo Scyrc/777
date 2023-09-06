386c6370926121b41c5eaa2b0f72366270f40aba
yrc <419107280@qq.com>
2023-09-06 20:48:09 +0800

    组合

---
+++ leetcode/leetcodeBook/brackTracking/39_CombinationSum.cc
@@ -0,0 +1,41 @@
+//
+// Created by sc on 2023/9/6.
+//
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+private:
+    vector<vector<int>> res;
+    void backTracking(const int& startIndex, vector<int>& select, const vector<int>& candidates,int& target)
+    {
+        if(target == 0)
+        {
+            res.push_back(select);
+        }
+
+        for (int i = startIndex; i < candidates.size(); ++i)
+        {
+            if(candidates[i] <= target)
+            {
+                target -= candidates[i];
+                select.push_back(candidates[i]);
+                backTracking(i, select, candidates, target);
+                select.pop_back();
+                target += candidates[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+public:
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> select;
+        sort(candidates.begin(), candidates.end());
+        backTracking(0, select, candidates, target);
+        return res;
+    }
+};
